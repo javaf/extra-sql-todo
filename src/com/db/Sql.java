a5d7e287150fff7bdee9c1244499ac2db9754ea6
Subhajit Sahu <wolfram77@gmail.com>
2015-02-25 17:41:34 +0530

    ready to test put functionality

--- src/com/db/Sql.java
+++ src/com/db/Sql.java
@@ -10,92 +10,81 @@ import org.data.*;
 
 
 
-public class Sql {
+public class Sql extends HashMap<String, Object> {
 
     // data
-    private Connection conn;
-    private String cmdStr;
-    private PreparedStatement cmd;
-    private Map<String, Object> fieldVal;
-    public Map<String, List<Integer>> fieldId;
+    String str;
+    Connection conn;
+    PreparedStatement cmd;
+    Map<String, List<Integer>> pos;
 
 
-    // Find And Replace (text, patternStr, replaceStr)
+    // FindAndReplace (text, patternStr, replaceStr)
     // - find the positions of given pattern in a list-map and replace it with given string
-    public static Map<String, List<Integer>> findAndReplace(StringBuilder text, String patternStr, String replaceStr) {
-        Map<String, List<Integer>> ans = new HashMap<String, List<Integer>>();
+    static Map<String, List<Integer>> posMap(StringBuilder text, String patternStr, String replaceStr) {
+        Map<String, List<Integer>> ans = new HashMap<>();
         Pattern pattern = Pattern.compile(patternStr);
         Matcher match = pattern.matcher(text.toString());
-        for (int id = 1, del = 0; match.find(); id++) {
-            int start = del + match.start(), end = del + match.end();
-            Coll.addToListMap(ans, text.substring(start + 1, end), id);
+        for (int id=1, del=0; match.find(); id++) {
+            int start = del+match.start(), end = del+match.end();
+            Coll.addToListMap(ans, text.substring(start+1, end), id);
             text.replace(start, end, replaceStr);
-            del += replaceStr.length() - (end - start);
+            del += replaceStr.length() - (end-start);
         }
         return ans;
     }
 
 
-    // Prep Cmd (cmdStr)
+    // Prepare (cmdStr)
     // - prepare sql command from given command string
-    private void prepCmd(String cmdStr) throws SQLException {
+    void prepare(String cmdStr) throws SQLException {
         StringBuilder cmdPrep = new StringBuilder(cmdStr);
-        fieldId = findAndReplace(cmdPrep, "(@)\\w+", "?");
+        pos = posMap(cmdPrep, "(@)\\w+", "?");
         cmd = conn.prepareStatement(cmdPrep.toString());
     }
 
 
-    // Cmd Field (key, val)
+    // SetField (key, val)
     // - set a @named_parameter with a value (use "\\(null)" for null value) (ignores unavailable value)
-    private void cmdField(String key, Object val) throws SQLException {
-        List<Integer> ids = fieldId.get(key);
-        if (ids == null) {
-            return;
-        }
+    private void setField(String key, Object val) throws SQLException {
+        List<Integer> ids = pos.get(key);
+        if (ids == null) return;
         for (Integer id : ids) {
-            if (val == null || val.equals("\\(null)")) {
-                cmd.setNull(id, Types.NULL);
-            } else if (val instanceof BigDecimal) {
-                cmd.setBigDecimal(id, (BigDecimal) val);
-            } else if (val instanceof java.util.Date) {
-                cmd.setDate(id, new java.sql.Date(((java.util.Date) val).getTime()));
-            } else {
-                cmd.setObject(id, val);
-            }
+            if (val == null || val.equals("\\(null)")) cmd.setNull(id, Types.NULL);
+            else if (val instanceof BigDecimal) cmd.setBigDecimal(id, (BigDecimal) val);
+            else if (val instanceof java.util.Date) cmd.setTimestamp(id, new Timestamp(((java.util.Date)val).getTime()));
+            else cmd.setObject(id, val);
         }
     }
 
 
-    // Cmd Clear Fields (key, val)
+    // ClearFields (key, val)
     // - clears all command fields that were set
-    private void cmdClearFields() throws SQLException {
+    private void clearFields() throws SQLException {
         cmd.clearParameters();
     }
 
 
-    // Constructor (conn)
+    // Sql (conn)
     // - set sql connection for command
     public Sql(Connection conn) {
         this.conn = conn;
-        fieldVal = new HashMap<String, Object>();
     }
 
 
-    // Cmd Str ()
-    // - get the set command string
-    public String cmdStr() {
-        return cmdStr;
+    // Str ()
+    // - get sql string
+    public String str() {
+        return str;
     }
 
 
-    // Cmd (cmdStr)
-    // - prepare command from command string
-    public Sql cmd(String cmdStr) throws SQLException {
-        if (cmdStr.equals(this.cmdStr)) {
-            return this;
-        }
-        this.cmdStr = cmdStr;
-        prepCmd(cmdStr);
+    // Str (str)
+    // - set sql string and prepare
+    public Sql str(String str) throws SQLException {
+        if (str.equals(this.str)) return this;
+        this.str = str;
+        prepare(str);
         return this;
     }
 
@@ -107,57 +96,33 @@ public class Sql {
     }
 
 
-    // Set (key, val)
+    // Put (key, val)
     // - set @named_parameter with given value
-    public Sql set(String key, Object val) throws SQLException {
-        fieldVal.put(key, val);
-        if (cmd != null) {
-            cmdField(key, val);
-        }
+    @Override
+    public Sql put(String key, Object val) {
+        System.out.println("put("+key+", "+val+")");
+        super.put(key, val);
+        if (cmd == null) return this;
+        try { setField(key, val); }
+        catch(Exception e) {}
         return this;
-    }
-
-
-    // Set (map)
-    // - set named parameters with given values
-    public Sql set(Map<String, Object> map) throws SQLException {
-        if (map == null) {
-            return this;
-        }
-        for (String key : map.keySet()) {
-            set(key, map.get(key));
-        }
-        return this;
-    }
-
-
-    // Get (key)
-    // - get field value from field name
-    public Object get(String key) {
-        return fieldVal.get(key);
-    }
-
-
-    // Get ()
-    // - get all fields
-    public Map<String, Object> get() {
-        return fieldVal;
     }
 
 
     // Clear ()
     // - clear all fields
-    public Sql clear() throws SQLException {
-        fieldVal.clear();
+    @Override
+    public void clear() {
+        super.clear();
         if (cmd != null) {
-            cmdClearFields();
+            try { clearFields(); }
+            catch (Exception e) {}
         }
-        return this;
     }
 
 
-    // Select Str (table, select fields, where fields)
-    // - get select command for given table and set fields
+    // SelectStr (table, select fields, where fields)
+    // - get select command for given table
     public static String selectStr(String table, Collection<String> selectFields, Collection<String> whereFields) {
         String selectPart = Coll.toString(selectFields, "\\(item)", ",");
         String wherePart = Coll.toString(whereFields, "\\(item)=@\\(item)", " AND ");
@@ -165,8 +130,8 @@ public class Sql {
     }
 
 
-    // Update Str (table, update fields, where fields)
-    // - get select command for given table and set fields
+    // UpdateStr (table, update fields, where fields)
+    // - get update command for given table
     public static String updateStr(String table, Collection<String> updateFields, Collection<String> whereFields) {
         String setPart = Coll.toString(updateFields, "\\(item)=@\\(item)", ",");
         String wherePart = Coll.toString(whereFields, "\\(item)=@\\(item)", " AND ");
@@ -174,8 +139,8 @@ public class Sql {
     }
 
 
-    // Insert Str (table)
-    // - get insert command for given table and set fields
+    // InsertStr (table, fields)
+    // - get insert command for given table
     public static String insertStr(String table, Collection<String> fields) {
         String intoPart = Coll.toString(fields, "\\(item)", ",");
         String valuesPart = Coll.toString(fields, "@\\(item)", ",");
